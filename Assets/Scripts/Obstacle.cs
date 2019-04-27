106858c59bd6f5163ec2197b85346ea8d64a1a0d
Eugene <evg_kod@mail.ru>
2019-04-27 13:50:13 +0700

    obstacle ontrigger enter

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -4,7 +4,8 @@ using EventSys;
 
 public class Obstacle : MonoBehaviour
 { 
-	private void OnCollisionEnter2D(Collision2D collision) { 
+	private void OnTriggerEnter2D() {
+		Debug.Log("trigger enter"); 
 		EventManager.Fire(new Event_Obstacle_Collided { Obstacle = this }); 
 	}
 }
