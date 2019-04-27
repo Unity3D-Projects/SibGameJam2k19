d2d9fda21e4df2d9c03c3d8b9c976d6cfc6fa75e
anton.grebennikov <mehozavr@gmail.com>
2019-04-27 11:59:28 +0700

    Контроллер персонажа и пара тестовых спрайтов

---
+++ Assets/Scripts/GoatController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using DG.Tweening;
+
+public class GoatController : MonoBehaviour {
+	Sequence _seq = null;
+
+	public PhysicsObject CharController = null;
+	private void Start() {
+	
+	}
+
+	private void Update() {
+		if ( Input.GetKeyDown(KeyCode.Space) ) {
+			Jump();
+		}
+	}
+
+	void Jump() {
+		CharController.Jump(5f);
+	}
+}
