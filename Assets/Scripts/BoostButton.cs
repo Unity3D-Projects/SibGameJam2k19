707aaf33e131ef71cf90f0d694c29f4dd09d526b
anton.grebennikov <anton.grebennikov@elefun-games.com>
2019-04-28 14:49:20 +0700

    Арт и всякое

--- Assets/Scripts/BoostButton.cs
+++ Assets/Scripts/BoostButton.cs
@@ -1,9 +1,15 @@
 using UnityEngine;
 using EventSys;
 
-public class BoostButton : MonoBehaviour
-{
+using TMPro;
+
+public class BoostButton : MonoBehaviour {
 	public BoostType BoostType;
+	public TMP_Text PriceText = null;
+
+	void Start() {
+		PriceText.text = GameState.Instance.BoostWatcher.GetBoostInfo(BoostType).Price.ToString();
+	}
 
 	public void BoostButtonClick() {
 		Debug.Log("Boost type used: " + BoostType);
